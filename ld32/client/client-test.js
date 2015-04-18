a4b31e6d921fcb093382c8ffde8108d5add5b767
cigumo <ciro@mondueri.com>
2015-04-18 20:08:05 -0300

    client test js

---
+++ ld32/client/client-test.js
@@ -0,0 +1,48 @@
+//var wsUri = "ws://echo.websocket.org/";
+//var wsUri = "ws://localhost:9999/";
+var wsUri = "ws://192.168.1.44:8080/";
+
+var output;
+function init() {
+    output = document.getElementById("output");
+    testWebSocket();
+}
+
+function testWebSocket() {
+    websocket = new WebSocket(wsUri);
+    websocket.onopen = function(evt) { onOpen(evt) };
+    websocket.onclose = function(evt) { onClose(evt) };
+    websocket.onmessage = function(evt) { onMessage(evt) };
+    websocket.onerror = function(evt) { onError(evt) };
+}
+
+function onOpen(evt) {
+    writeToScreen("CONNECTED");
+    doSend("WebSocket rocks");
+}
+
+function onClose(evt) {
+    writeToScreen("DISCONNECTED");
+}
+function onMessage(evt) {
+    writeToScreen('<span style="color: blue;">RESPONSE: ' + evt.data+'</span>');
+    //websocket.close();
+}
+
+function onError(evt) {
+    writeToScreen('<span style="color: red;">ERROR:</span> ' + evt.data);
+}
+
+function doSend(message) {
+    writeToScreen("SENT: " + message);
+    websocket.send(message);
+}
+
+function writeToScreen(message) {
+    var pre = document.createElement("p");
+    pre.style.wordWrap = "break-word";
+    pre.innerHTML = message;
+    output.appendChild(pre);
+}
+
+window.addEventListener("load", init, false);
